1d4a3020c4af247c2dbcd57b023f0da454ffb9b4
lyraxvincent <njongevincent@gmail.com>
2020-10-07 15:33:35 +0300

    add method to use the most relevant topics

--- get_tweets_by_topic.py
+++ get_tweets_by_topic.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import authconfig
 import pandas as pd
+import random
 
 # tweepy authentication
 
@@ -46,6 +47,16 @@ def tweets_by_word_search(word):
 topics = pd.read_csv("csv files/topics.csv")
 topics = list(topics.topics)
 
-
+# pick the most relevant topics
+val_topics = []
 for topic in topics:
+    if topics.count(topic) > 1:     # first pick those with occurrence >1
+        val_topics.append(topic)
+        topics[:] = [item for item in topics if item != topic]      # remove element from list to avoid adding duplicate value in loop below
+
+# ensure we have 5 topics
+while len(val_topics) < 5:
+    val_topics.append(random.choice(topics[-5:]))   # pick from the last 5 elements(last collected set)
+
+for topic in val_topics:
     tweets_by_word_search(topic)
