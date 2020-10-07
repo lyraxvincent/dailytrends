70897c64c4e0a7ffbc1fc2ada9b988b9520c99cd
lyraxvincent <njongevincent@gmail.com>
2020-10-07 16:42:58 +0300

    refactor code -correcting errors

--- dailytrends.py
+++ dailytrends.py
@@ -1,27 +1,46 @@
 import schedule
 import time
 
+# scripts
+def delprevfiles():
+    import delprevfiles
+
+def get_topics():
+    import get_topics
+
+def get_tweets_by_topic():
+    import get_tweets_by_topic
+
+def sort_csv():
+    import sort_csv
+
+def cleanfiles():
+    import cleanfiles
+
+def send_news_to_mail():
+    import send_news_to_mail
+
 # delete previously used topic files
-schedule.every().day.at("06:00").do(python3 get_topics.py)
+schedule.every().day.at("06:00").do(delprevfiles)
 
 # collect topics
-schedule.every().day.at("07:00").do(python3 get_topics.py)
-schedule.every().day.at("09:00").do(python3 get_topics.py)
-schedule.every().day.at("11:00").do(python3 get_topics.py)
-schedule.every().day.at("13:00").do(python3 get_topics.py)
-schedule.every().day.at("15:00").do(python3 get_topics.py)
+schedule.every().day.at("07:00").do(get_topics)
+schedule.every().day.at("09:00").do(get_topics)
+schedule.every().day.at("11:00").do(get_topics)
+schedule.every().day.at("13:00").do(get_topics)
+schedule.every().day.at("15:00").do(get_topics)
 
 # collect tweets
-schedule.every().day.at("15:30").do(python3 get_tweets_by_topic.py)
+schedule.every().day.at("15:30").do(get_tweets_by_topic)
 
 # sort csv files
-schedule.every().day.at("18:00").do(python3 sort_csv.py)
+schedule.every().day.at("18:00").do(sort_csv)
 
 # clean csv files
-schedule.every().day.at("18:05").do(python3 cleanfiles.py)
+schedule.every().day.at("18:05").do(cleanfiles)
 
 # send summary news to users
-schedule.every().day.at("18:10").do(python3 send_news_to_mail.py)
+schedule.every().day.at("18:10").do(send_news_to_mail)
 
 while True:
     schedule.run_pending()
